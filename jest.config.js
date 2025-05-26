bf07ac3aa1620a90d87b7c8dabf09949e2857d67
ckomiya <christian.komiya@gmail.com>
2025-05-26 09:35:00 -0500

    correccion de js en jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 export default {
-  modulePathIgnorePatterns: ['<rootDir>/node_test/'],
-  coveragePathIgnorePatterns: ['<rootDir>/tests/'],
+  modulePathIgnorePatterns: ["<rootDir>/node_test/"],
+  coveragePathIgnorePatterns: ["<rootDir>/tests/"],
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
-  testEnvironment: 'node'
-}
+  testEnvironment: "node",
+};
